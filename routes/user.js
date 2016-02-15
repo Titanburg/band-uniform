98fe24fbb151ddbd915f6561e6d43c6b030167df
Kyle Walter <walter.kl26@gmail.com>
2016-02-14 23:11:46 -0600

    Authentication working

--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,17 @@ module.exports = function(passport){
   var express = require('express');
   var router = express.Router();
   /* GET users listing. */
-  router.get('/signup',passport.authenticate('local-signup', {
+  router.get('/signup',function(req,res,next){
+    res.render('signup',{title: 'Signup',message: req.flash('signupMessage')});
+  });
+  router.post('/signup',passport.authenticate('local-signup', {
     successRedirect: '/', // redirect to the secure profile section
     failureRedirect: '/signup', // redirect back to the signup page if there is an error
     failureFlash: true // allow flash messages
   }));
 
   router.get('/login',function(req,res,next){
-    res.render('login');
+    res.render('login',{title: 'Login',message: req.flash('loginMessage')});
   });
   router.post('/login',passport.authenticate('local-login', {
     successRedirect: '/dashboard', // redirect to the secure profile section
