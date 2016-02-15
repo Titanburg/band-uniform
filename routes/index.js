3b60e264ba5f790746a0137d0287f756d8346ee7
Kyle Walter <walter.kl26@gmail.com>
2016-02-14 22:56:21 -0600

    Commented out api references in routing index

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@
 module.exports = function(app,passport){
 
   var user = require('./user')(passport);
-  var api = require('./api');
+  // var api = require('./api');
 
   app.use('/user',user);
-  app.use('/api',api);
+  // app.use('/api',api);
 
   app.get('/dashboard', isLoggedIn,function(req,res,next){
     res.render('index',{title: 'Titanburg Band Uniform'});
