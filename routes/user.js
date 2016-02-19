81a5c5e221d539c32d814bf428141bab674c0fe6
Kyle Walter <walter.kl26@gmail.com>
2016-02-18 23:35:16 -0600

    Fixed login button signup fail route

--- routes/user.js
+++ routes/user.js
@@ -7,8 +7,8 @@ module.exports = function(passport){
     res.render('signup',{title: 'Signup',message: req.flash('signupMessage')});
   });
   router.post('/signup',passport.authenticate('local-signup', {
-    successRedirect: '/', // redirect to the secure profile section
-    failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    successRedirect: '/dashboard', // redirect to the secure profile section
+    failureRedirect: '/user/signup', // redirect back to the signup page if there is an error
     failureFlash: true // allow flash messages
   }));
 
