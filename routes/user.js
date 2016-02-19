33eb96cb8fecb5475f4768e3bb0a73d116985808
Kyle Walter <walter.kl26@gmail.com>
2016-02-18 19:45:08 -0600

    Created user logout. Checks if already logged in when requesting /user/login rout

--- routes/user.js
+++ routes/user.js
@@ -12,14 +12,25 @@ module.exports = function(passport){
     failureFlash: true // allow flash messages
   }));
 
-  router.get('/login',function(req,res,next){
+  router.get('/login',isLoggedOut,function(req,res,next){
     res.render('login',{title: 'Login',message: req.flash('loginMessage')});
   });
-  router.post('/login',passport.authenticate('local-login', {
+  router.post('/login',isLoggedOut,passport.authenticate('local-login', {
     successRedirect: '/dashboard', // redirect to the secure profile section
     failureRedirect: '/user/login', // redirect back to the signup page if there is an error
     failureFlash: true // allow flash messages
   }));
+
+  router.get('/logout',function(req,res,next){
+    req.logout();
+    res.redirect('/user/login');
+  });
+
+  function isLoggedOut(req, res, next) {
+    if (!req.isAuthenticated())
+      return next();
+    res.redirect('/dashboard');
+  }
 
   return router;
 };
