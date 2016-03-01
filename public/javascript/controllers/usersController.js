cf6c26263b425e00aaf38936f83afa13e1194e5f
Kyle Walter <walter.kl26@gmail.com>
2016-02-29 23:23:54 -0600

    Added users variable to populate page.

--- public/javascript/controllers/usersController.js
+++ public/javascript/controllers/usersController.js
@@ -2,6 +2,15 @@
  * Created by Kyle Walter on 2/29/2016.
  */
 angular.module('bandApp')
-    .controller(function($scope){
-        $scope.title = "This is the users page."
+    .controller(function($scope,$http){
+        $scope.title = "This is the users page.";
+        $scope.users = [];
+        $scope.getUsers = function(){
+          $http.get('/api/user')
+            .success(function(data){
+              $scope.users = data;
+            }).error(function(err){
+              console.log(err);
+            })
+        };
     });
