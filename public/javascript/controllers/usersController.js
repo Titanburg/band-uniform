57ab363938c87c54333686f4f951f48e2329ec4d
Kyle Walter <walter.kl26@gmail.com>
2016-02-29 20:45:22 -0600

    Fixed usersController

--- public/javascript/controllers/usersController.js
+++ public/javascript/controllers/usersController.js
@@ -1,7 +1,7 @@
 /**
  * Created by Kyle Walter on 2/29/2016.
  */
-angular.module('bandApp',[])
+angular.module('bandApp')
     .controller(function($scope){
         $scope.title = "This is the users page."
     });
