ddcfe8033de690224b04bec3ce91c2e892ff88da
Kyle Walter <walter.kl26@gmail.com>
2016-02-29 20:01:41 -0600

    Added $locationProvider which will take out the '#" that is normally at the bigging of the route when using angular

--- public/javascript/bandApp.js
+++ public/javascript/bandApp.js
@@ -1,10 +1,11 @@
 
 // bandApp angular module creation
 angular.module('bandApp',['ngRoute'])
-    .config(function($routeProvider){
+    .config(function($routeProvider,$locationProvider){
         $routeProvider
             .when('/users',{
                 templateUrl: 'partials/users.jade',
                 controller: 'usersController'
             });
+        $locationProvider.html5Mode(true);
     });
