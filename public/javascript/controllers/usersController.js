338791936dcbb6505348593b0894b39a6490d75c
Kyle Walter <walter.kl26@gmail.com>
2016-02-29 21:40:34 -0600

    Added main table view to the users.jade. Added modal Added dumby data to usersController

--- public/javascript/controllers/usersController.js
+++ public/javascript/controllers/usersController.js
@@ -4,4 +4,26 @@
 angular.module('bandApp')
     .controller('usersController',function($scope){
         $scope.title = "This is the users page."
+        $scope.users = [
+            {
+                firstName:'Kyle',
+                lastName:'Walter',
+                email:'walter.kl26@gmail.com'
+            },
+            {
+                firstName:'Kirk',
+                lastName:'Powell',
+                email:'kirk@thegamestoreapp.com'
+            },
+            {
+                firstName:'Andrew',
+                lastName:'Korous',
+                email:'AJkorous@gmail.com'
+            },
+            {
+                firstName:'Kaitlyn',
+                lastName:'Jarrett',
+                email:'kjarrett316@gmail.com'
+            }
+        ];
     });
