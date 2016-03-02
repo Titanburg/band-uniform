c2b3aeaaad1af487fbfc5a8aabbc5ed2acdb1295
Kaitlyn Jarrett <kjarrett316@gmail.com>
2016-03-02 12:32:49 -0600

    Added models/maintenance_request.js, public/javascript/controllers/maintenanceController, and updated index.jade, bandApp.js and layout.jade.

--- public/javascript/bandApp.js
+++ public/javascript/bandApp.js
@@ -6,6 +6,10 @@ angular.module('bandApp',['ngRoute'])
             .when('/users',{
                 templateUrl: 'partial/users.jade',
                 controller: 'usersController'
+            })
+            .when('/maintenance_request',{
+                templateUrl: 'partial/maintenance_request.jade',
+                controller: 'maintenanceController'
             });
         $locationProvider.html5Mode(true);
     });
