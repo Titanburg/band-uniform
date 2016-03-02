f2c2640e1c800f3b2112b314c722b74cdb5f919f
Kyle Walter <walter.kl26@gmail.com>
2016-03-02 23:33:31 +0000

    Admin can now add password when creating or change password of existing user.

--- controllers/user.js
+++ controllers/user.js
@@ -32,7 +32,7 @@ module.exports = {
                 newUser.local.firstName = req.body.local.firstName;
                 newUser.local.lastName = req.body.local.lastName;
                 newUser.local.admin = req.body.local.admin;
-                // newUser.local.password = newUser.generateHash(req.body.password);
+                newUser.local.password = newUser.generateHash(req.body.local.password);
                 newUser.save(function(err) {
                     if (err)
                         throw err;
@@ -70,9 +70,9 @@ module.exports = {
                 user.local.firstName = req.body.local.firstName;
                 user.local.lastName = req.body.local.lastName;
                 user.local.admin = req.body.local.admin;
-                //if(req.body.password != ''){
-                //    user.local.password = user.generateHash(req.body.password);
-                //}
+                if(req.body.local.password != ''){
+                    user.local.password = user.generateHash(req.body.local.password);
+                }
                 user.save(function(err) {
                     if (err)
                         throw err;
