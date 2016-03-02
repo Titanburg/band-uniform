cf0db2ea623bab1a49517178c007dee07f0af21f
Kaitlyn Jarrett <Kaitlyn@wlcl251117.wlcl.siu.edu>
2016-03-02 12:32:49 -0600

    Added models/maintenance_request.js, public/javascript/controllers/maintenanceController, and updated index.jade, bandApp.js and layout.jade.

---
+++ controllers/maintenance_request.js
@@ -0,0 +1,15 @@
+var request = require('../models/maintenance_request.js');
+
+module.exports = {
+    getRequests : function(req,res){
+        request.find({},function(err,maintenance_request){
+            if(err) return;
+            console.log(requests);
+            res.json(requests);
+        });
+    },
+    newRequest : function(req,res){},
+    getRequest : function(req,res){},
+    editRequest : function(req,res){},
+    deleteRequest :function(req,res){}
+};
