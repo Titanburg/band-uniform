783d7cc3e7e6e04ce2709498ce99c623fcc2e17b
Kyle Walter <walter.kl26@gmail.com>
2016-03-02 23:09:13 +0000

    All crud functions work and lists updates automatically when change is made

--- public/javascript/controllers/usersController.js
+++ public/javascript/controllers/usersController.js
@@ -24,14 +24,17 @@ angular.module('bandApp')
             $scope.creating = true;
             $scope.user = {};
         };
+        
         $scope.getUsers = function(){
           $http.get('/api/user')
             .success(function(data){
+              console.log(data);
               $scope.users = data;
             }).error(function(err){
               console.log(err);
             });
         };
+        
         $scope.getUser = function(user){
             $http.get('/api/user/' + user._id )
                 .success(function(data){
@@ -40,11 +43,12 @@ angular.module('bandApp')
                 console.log(err);
             });
         };
+        
         $scope.sendUser = function(){
             if($scope.creating === true){
                $http.post('/api/user',$scope.user)
                    .success(function(data){
-                     
+                     $scope.users = data;
                    }).error(function(err){
                    console.log(err);
                });
@@ -52,18 +56,19 @@ angular.module('bandApp')
             }else{
               $http.post('/api/user/' + $scope.user._id,$scope.user)
                   .success(function(data){
-                    
+                    $scope.users = data;
                   }).error(function(err){
                   console.log(err);
               });
             }
-            $scope.getUsers();
+            
 
         };
+        
         $scope.deleteUser = function(user){
           $http.get('/api/user/delete/' + user._id )
               .success(function(data){
-                
+                $scope.users = data;
               }).error(function(err){
               console.log(err);
           });
