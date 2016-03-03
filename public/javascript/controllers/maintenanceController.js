98f220973bd6fe7482a8e34048d113012a4f1ace
Kaitlyn Jarrett <Kaitlyn@wlcl119152.wlcl.siu.edu>
2016-03-03 09:07:51 -0600

    Can add/edit requests but cannot delete.

--- public/javascript/controllers/maintenanceController.js
+++ public/javascript/controllers/maintenanceController.js
@@ -1,7 +1,11 @@
 angular.module('bandApp')
     .controller('maintenanceController',function($scope,$http){
-        $scope.title = "This is the maintenance page.";
+        $scope.title = "Maintenance Requests";
+        $scope.creating = false;
+        $scope.request = {};
         $scope.requests = [];
+
+
         $scope.getRequests = function(){
           $http.get('/api/maintenance_request')
             .success(function(data){
@@ -9,5 +13,64 @@ angular.module('bandApp')
             }).error(function(err){
               console.log(err);
             });
+        };
+
+        $scope.createRequest = function(){
+            $scope.creating = true;
+            $scope.request = {};
+        };
+
+        $scope.getRequests = function(){
+          $http.get('/api/maintenance_request')
+            .success(function(data){
+              console.log(data);
+              $scope.requests = data;
+            }).error(function(err){
+              console.log(err);
+            });
+        };
+
+        $scope.getRequest = function(request){
+            $http.get('/api/maintenance_request/' + request._id )
+                .success(function(data){
+                    $scope.request = data;
+                }).error(function(err){
+                console.log(err);
+            });
+        };
+
+        $scope.sendRequest = function(){
+            // If creating is true send new user
+            if($scope.creating === true){
+               $http.post('/api/maintenance_request',$scope.request)
+                   .success(function(data){
+                     $scope.requests = data;
+                   }).error(function(err){
+                   console.log(err);
+               });
+            }
+            // If creating is false edit existing user
+            else{
+              $http.post('/api/maintenance_request/' + $scope.request._id,$scope.request)
+                  .success(function(data){
+                    $scope.requests = data;
+                  }).error(function(err){
+                  console.log(err);
+              });
+            }
+
+            //Disable creating variable to allow
+            $scope.creating = false;
+            //Clear all data in user variable. This is for security.
+            $scope.request = {};
+        };
+
+        $scope.deleteRequest = function(request){
+            $http.get('/api/request/delete/' + request._id )
+            .success(function(data){
+              $scope.requests = data;
+            }).error(function(err){
+            console.log(err);
+          });
         };
     });
