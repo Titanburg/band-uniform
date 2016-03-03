8b28b64ee2473d934c225b287e6e34d9300d2508
AndrewKorous <ajkorous@gmail.com>
2016-03-03 00:23:26 -0600

    Modal works, updated login page a tad.

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,6 @@ module.exports = function(passport){
   var express = require('express');
   var router = express.Router();
   /* GET users listing. */
-  router.get('/signup',function(req,res,next){
-    res.render('signup',{title: 'Signup',message: req.flash('signupMessage')});
-  });
   router.post('/signup',passport.authenticate('local-signup', {
     successRedirect: '/', // redirect to the secure profile section
     failureRedirect: '/auth/signup', // redirect back to the signup page if there is an error
