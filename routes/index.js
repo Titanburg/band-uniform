a1f9bf4313b47a42869a3e90527fc874cc01e679
Kyle Walter <walter.kl26@gmail.com>
2016-03-28 14:43:34 -0500

    Permission structure is setup for views. Current views are implimented

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,29 @@ module.exports = function(app,passport){
   app.use('/api',isLoggedIn,api);
 
   app.get('/partial/:name', isLoggedIn,function(req,res,next){
-    res.render('partials/' + req.params.name);
+    if(req.user && req.user.local.admin){
+      console.log(req.params.name);
+      switch(req.params.name){
+        case 'users':
+        case 'maintenance_request':
+          res.render('partials/admin/' + req.params.name);
+          break;
+        default:
+          res.render('partials/oops');
+      }
+    }else{
+      switch(req.params.name){
+        case 'maintenance_request':
+          res.render('partials/admin/' + req.params.name);
+          break;
+        default:
+          res.render('partials/oops');
+      }
+    }
   });
 
   app.get('/*',isLoggedIn,function(req,res,next){
-    res.render('index',{title:'Band Uniform Management Utility',user:{name:req.user.local.firstName}});
+    res.render('index',{title:'Band Uniform Management Utility',user:{email:req.user.local.email,admin:req.user.local.admin}});
   });
 
   function isLoggedIn(req, res, next) {
