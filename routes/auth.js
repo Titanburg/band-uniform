5033f25eabb24f916a56919e84279b5bbf7e160f
Kyle Walter <walter.kl26@gmail.com>
2016-04-30 15:57:30 -0500

    fixed message again

--- routes/auth.js
+++ routes/auth.js
@@ -26,19 +26,19 @@ module.exports = function(passport){
     if (err) { return next(err); }
     if (!user) {
       if(req.body.type === 'android')
-        return res.json({Status:{
-          login:'FAILURE',
+        return res.json({
+          status:'FAILURE',
           message:req.flash('loginMessage')
-        }});
+        });
       return res.redirect('/auth/login');
     }
     req.logIn(user, function(err) {
       if (err) { return next(err); }
       if(req.body.type === 'android')
-        return res.json({Status:{
-          login:'SUCCESS',
+        return res.json({
+          status:'SUCCESS',
           message:''
-        }});
+        });
       return res.redirect('/users');
     });
   })(req, res, next);
