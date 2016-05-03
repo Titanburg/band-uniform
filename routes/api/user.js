bd446797821225bac9f84bf8d90801be2f934dd0
AndrewKorous <ajkorous@gmail.com>
2016-05-02 19:21:24 -0500

    Begininng of NodeMailer implemented

--- routes/api/user.js
+++ routes/api/user.js
@@ -5,12 +5,40 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../../controllers/user.js');
+var nodemailer = require('nodemailer');
 
 router.get('/',isAdmin,controller.getUsers);
 router.post('/',isAdmin,controller.newUser);
 router.get('/:id',controller.getUser);
 router.post('/:id',controller.editUser);
 router.get('/delete/:id',isAdmin,controller.deleteUser);
+
+router.post('/confirm', function(req, res, next){
+  var transporter = nodeMailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'SalukiUniforms@gmail.com',
+      pass: 'titanburg'
+    }
+  });
+
+  var mailOptions = {
+    from: /*firstName from session*/ /*lastName from session*/ <SalukiUniforms@gmail.com>,
+    to: //user email that was approved
+    subject: 'Band Uniform Account Confrimed',
+    text: 'Your account on the SIU Band Uniform app has been approved and is now active!',
+    html: '<p> Your account on the SIU Band Uniform app has has been approved and is now active!</p>'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.log(error);
+    }
+    else {
+      console.log('Message sent <3' + info.response);
+    }
+  });
+})
 
 function isAdmin(req,res,next){
   if(req.user && req.user.local.admin){
