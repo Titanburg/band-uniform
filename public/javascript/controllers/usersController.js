7d8eaa48fb59904f1e1270136252b724e3924ba6
AndrewKorous <ajkorous@gmail.com>
2016-05-02 21:56:05 -0500

    Damn thing refuses to post, cant even test the nodemailer part errgegrgergqijewbfkhbqr;lkgjq'elrkj

--- public/javascript/controllers/usersController.js
+++ public/javascript/controllers/usersController.js
@@ -130,9 +130,23 @@ angular.module('bandApp')
         $scope.activateUser = function(user,state){
             $scope.getUser(user,function(){
                 $scope.user.local.state = state;
-                $scope.sendUser(true, user);
+                $scope.sendUser(true, user,function(err){
+                  console.log('her saved');
+                });
+
             });
+            $scope.sendEmailConfirmation(user);
         };
+
+        $scope.sendEmailConfirmation = function(user) {
+          $http.post('/api/user/sendConfirmation', user)
+            .success(function(data){
+              console.log("I think email sent? ...maybe??");
+            })
+            .error(function(error){
+              console.log(error);
+            });
+        }
 
         // Crud Functions
         $scope.createUser = function(){
@@ -158,20 +172,22 @@ angular.module('bandApp')
                 .success(function(data){
                     $scope.user = data;
                     if($scope.user.local && $scope.user.local.state) $scope.user.local.state = $scope.user.local.state.toString();
-                    callback(null);
+                    callback();
                 }).error(function(err){
                 callback(err);
             });
         };
-        $scope.sendUser = function(isValid, user){
+        $scope.sendUser = function(isValid, user,callback){
           if(isValid){
             // If creating is true send new user
             if($scope.creating === true){
                $http.post('/api/user',$scope.user)
                    .success(function(data){
                      $scope.users = data;
+                    //  callback();
                    }).error(function(err){
                    console.log(err);
+                   callback(err);
                });
             }
             // If creating is false edit existing user
@@ -179,12 +195,10 @@ angular.module('bandApp')
               $http.post('/api/user/' + $scope.user._id,$scope.user)
                   .success(function(data){
                     $scope.users = data;
-                    $http.post('/sendConfirmation', user)
-                      .success(function(data){
-                        console.log("I think email sent? ...maybe??");
-                      });
+                    // callback();
                   }).error(function(err){
                   console.log(err);
+                  callback(err);
               });
             }
 
--- routes/api/user.js
+++ routes/api/user.js
@@ -5,43 +5,14 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../../controllers/user.js');
-var nodemailer = require('nodemailer');
+
 
 router.get('/',isAdmin,controller.getUsers);
 router.post('/',isAdmin,controller.newUser);
 router.get('/:id',controller.getUser);
 router.post('/:id',controller.editUser);
 router.get('/delete/:id',isAdmin,controller.deleteUser);
-
-router.post('/sendConfirmation', function(req, res, next){
-  var transporter = nodeMailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: 'SalukiUniforms@gmail.com',
-      pass: 'titanburg'
-    }
-  });
-
-  console.log(req.session);
-  console.log(req.body.email);
-
-  var mailOptions = {
-    from: '/*firstName from session*/ /*lastName from session*/ <SalukiUniforms@gmail.com>',
-    to: '//user email that was approved',
-    subject: 'Band Uniform Account Confirmed',
-    text: 'Your account on the SIU Band Uniform app has been approved and is now active! Login at titanburg.me to set up your account',
-    html: '<p>Your account on the SIU Band Uniform app has has been <b>approved</b> and is now active! Login at titanburg.me to set up your account</p>'
-  };
-
-  transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-      console.log(error);
-    }
-    else {
-      console.log('Message sent <3' + info.response);
-    }
-  });
-})
+router.post('/sendConfirmation', controller.emailConfirmation);
 
 function isAdmin(req,res,next){
   if(req.user && req.user.local.admin){
